7fa06fa693b9132c38f2bdd9579b908547ab1463
Greg Zahora <gregzahora3@gmail.com>
2019-07-09 18:55:57 -0400

    added functions to add butons from search box as well as to display gif and rating when button is selected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,44 @@
 //Initial Array of Politicians
 var topics = ["Elizabeth Warren", "Joe Biden", "Cory Booker", "Kamala Harris", "Pete Buttigieg", "Bill de Blasio", "Julian Castro", "Bernie Sanders"];
+for (var j = 0; j < topics.length; j++){
 
-//Function to  allow html display gifs and rating
-    
-    var politician = $(this).attr("data-name");
+}
+
+politicianButtons();
+
+ // Function for displaying buttons
+ function politicianButtons() {
+    $("#gifButtons").empty();
+    for (var i = 0; i < topics.length; i++){
+        $("#gifButtons").append("<button class='btn btn-info m-2 topicButton' data-name=" + topics[i] + ">" + topics[i] + "</button>");
+    }
+};
+
+
+//function display Gifs
+  $(document).on("click",".topicButton", function(){
+      $("#gifDisplay").empty();
+    var politician = $(this).data("name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + politician + "&api_key=uCO577Cm4JScLTXEm3bCjg2b636mQ8dq&limit=10";
 
     $.ajax({
-    url: queryURL,
-    method: "GET"
+        url: queryURL,
+        method: "GET"
     }).then(function(response) {
-    console.log(response);
+        console.log(response);
+
+        for(var i = 0; i < response.data.length; i++){
+    
+        $("#gifDisplay").append("<p>" + response.data[i].rating + "</p>")
+        $("#gifDisplay").append("<img src =" + response.data[i].images.fixed_height.url + "</img>")
+        }
     });
+  });  
+
+  $("#addButton").on("click", function(event) {
+    event.preventDefault();
+    var gif = $("input").val().trim();
+    topics.push(gif);
+    politicianButtons();
+    return false;
+  });
