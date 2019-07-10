3e69060e6af799cc66938f336ea2cc55c080479e
Greg Zahora <gregzahora3@gmail.com>
2019-07-10 16:24:27 -0400

    added ability to swtich gifs between still and animated state

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,7 @@
 //Initial Array of Politicians
 var topics = ["Elizabeth Warren", "Joe Biden", "Cory Booker", "Kamala Harris", "Pete Buttigieg", "Bill de Blasio", "Julian Castro", "Bernie Sanders"];
-for (var j = 0; j < topics.length; j++){
 
-}
-
-politicianButtons();
-
- // Function for displaying buttons
+ // Function for displaying buttons that first clears the div holding the buttons, then re-adds what is in the topic array
  function politicianButtons() {
     $("#gifButtons").empty();
     for (var i = 0; i < topics.length; i++){
@@ -14,33 +9,64 @@ politicianButtons();
       }
 };
 
+//function to add a new politican to the buttons after typing it in the input box
+$("#addButton").on("click", function(event) {
+  event.preventDefault();
+  var gif = $("input").val().trim();
+  topics.push(gif);
+  politicianButtons();
+  return false;
+});
 
-//function display Gifs
+
+//function display Gifs when clicking the button
   $(document).on("click",".topicButton", function(){
       $("#gifDisplay").empty();
     var politician = $(this).attr("data-person");
     console.log(politician);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + politician + "&api_key=uCO577Cm4JScLTXEm3bCjg2b636mQ8dq&limit=10";
 
+    //Ajax call for Giphy API
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
 
+        //loop to get gifs for each politician
         for(var i = 0; i < response.data.length; i++){
-    
-        $("#gifDisplay").append("<p>" + response.data[i].rating + "</p>")
-        $("#gifDisplay").append("<img src =" + response.data[i].images.fixed_height.url + "</img>")
+
+        //variables needed for still and animated gifs
+        var still = response.data[i].images.fixed_height_still.url;
+        var animate = response.data[i].images.fixed_height.url;
+        
+        //display gifs in their still version, but add attributes for the animated version and for the ability of the next function to switch between the two
+        $("#gifDisplay").append("<img src ='" + still + "' data-still='" + still + "' data-animate='" + animate + "' data-state='still' class='changeState'></img>")
+        
+        //display the gif ratings
+        $("#gifDisplay").append("<p><strong>Rating: " + response.data[i].rating + "</strong></p>")
         }
     });
-  });  
+  }); 
 
-//function to add a new politican to the buttons after typing it in the input box
-  $("#addButton").on("click", function(event) {
-    event.preventDefault();
-    var gif = $("input").val().trim();
-    topics.push(gif);
-    politicianButtons();
-    return false;
-  });
+
+  
+//function to pause/play gif by clicking on the gif
+$(document).on("click",".changeState", function() {
+  
+  //create variable to hold the gif's data-state
+  var state = $(this).attr("data-state");
+
+  //if the data-state is still the when you click it change the source to the animated version and change the data-state to animate and vice versa
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});  
+
+
+// Immediately display the pre-set politican buttons
+politicianButtons();
